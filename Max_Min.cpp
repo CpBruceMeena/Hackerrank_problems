044157018e7fd0c670365cb2649d9457e1433e83
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-07-23 10:07:38 +0530

    Create Max_Min.cpp

---
+++ Max_Min.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the maxMin function below.
+int maxMin(int k, vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    int max = -1, min = INT_MAX;
+    min = arr[0], max = arr[k-1];
+    int ans = max - min;
+    for(int i = k; i<arr.size(); i++){
+        max = arr[i];
+        min = arr[i-k+1];
+        if(max - min < ans) ans = max - min;
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item;
+        cin >> arr_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        arr[i] = arr_item;
+    }
+
+    int result = maxMin(k, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
