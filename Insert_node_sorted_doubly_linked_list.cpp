e619db9a4754a0a686a5549fc07d6e5adb0d4ce7
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-08-02 23:09:50 +0530

    Create Insert_node_sorted_doubly_linked_list.cpp

---
+++ Insert_node_sorted_doubly_linked_list.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    
+    DoublyLinkedListNode* result = head;
+    DoublyLinkedListNode* temp = new DoublyLinkedListNode(data);
+    if(head == NULL) return temp;
+    if(head->data > data){
+        temp->next = head;
+        head->prev = temp;
+        return temp;
+    }
+
+    while(head->next != NULL and head->next->data < data){
+        head = head->next;
+    }
+    if(head->next == NULL and head->data < data){
+        head->next = temp;
+        temp->next = NULL;
+        temp->prev = head;
+        return result;
+    }
+
+    temp->next = head->next;
+    head->next->prev = temp;
+    temp->prev = head;
+    head->next = temp;
+    return result;
+}
+
+int main()
